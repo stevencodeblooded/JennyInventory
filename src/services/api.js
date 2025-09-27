6eea7b301a0ea9ce4009cca158a6cae4a8f78069
Steven Ochieng <steven.ochieng432@gmail.com>
2025-09-27 14:33:32 +0300

    For Upload

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1";
+  process.env.REACT_APP_API_URL || "https://shop-inventory-five.vercel.app/api/v1";
 
 // Create axios instance
 const api = axios.create({
